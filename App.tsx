66579e4aea5a22f1655e7c221f759706a2173579
John Doe <pedro.rocha2609@gmail.com>
2023-06-30 10:53:09 -0300

    fim

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
-import { StatusBar, SafeAreaView } from 'react-native';
+import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 
-import { Home } from '@screens/Home';
 
 import { AuthContextProvider } from "@contexts/AuthContext";
 
